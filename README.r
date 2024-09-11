c3bf0455bcdd930ed254afe798b40189023323c4
Charlie Lindgren <clg@du.se>
2024-09-11 12:11:40 +0200

    Add .gitignore file and ignore node_modules and .Rproj.user

---
+++ README.r
@@ -0,0 +1,117 @@
+#' # Assignment Instruction - Bootstrap 5
+#' `r Sys.Date()`
+#+ echo = FALSE
+#'
+#' ## Overview
+#'
+#' In this assignment, you will be working with a pre-built HTML template of a
+#' website that uses Bootstrap 5 to promote a fictional course. 
+#' 
+#' Your task is to customize the template by adding content, styling, and
+#' functionality to create a unique and engaging webpage that showcases the
+#' course. You will be required to add certain elements to the template, such as
+#' text, images, buttons, and navigation links, and modify the existing styles
+#' to match the theme of the course.
+#' 
+#' You will also be required to make sure that certain classes are restored so
+#' as to resolve issues where functionality is broken.
+#' 
+#' Assignment details and minimum requirements to pass are outlined below. You
+#' may use this project style or syntax as inspiration for your own projects.
+#'
+#' ## Assignment Details
+#' 
+#' - **Title:** Customizing a Bootstrap 5 Website
+#' 
+#' - **Objective:** To customize a pre-built HTML template using Bootstrap 5
+#' 
+#' - **Requirements:** You will need to add content, styling, and functionality
+#' 
+#' - **Submission:** You will submit a .zip file containing the modified HTML
+#'   file
+#' 
+#' ## Getting started
+#' 
+#' 1. Clone the repository containing the pre-built HTML template or download
+#'    the .zip file.
+#' 
+#' 2. Extract the contents of the .zip file to a local directory on your
+#'    computer or make a local copy of the repository using Git.
+#'
+#' 3. Open the `index.html` file in a text editor or code editor of your choice.
+#'
+#' - **Important note**: You will exclusively work with the `index.html` file.
+#'    Do not modify any other files in the template. If you want to "play
+#'    around" with the template, such as the .css or .scss files, make a copy of
+#'    the project and work on that copy in parallel or use it for reference in
+#'    your own projects. For the purpose of this assignment, further broken
+#'    functionality should be avoided by maintaining current styles.
+#' 
+#' 4. Customize the template by adding content, styling, and functionality to
+#'   create a unique and engaging webpage that showcases the course. The minimum
+#'   requirements to pass the assignment are laid out in the next section.
+#' 
+#' - **Important note**: Add comments, e.g., `<!-- Your content here -->`, to
+#'  indicate where you have made changes or added new elements to the template.
+#' 
+#' 5. Once you have completed customizing the template, save the `index.html`
+#' 
+#' 6. Create a ZIP file containing the `index.html` file only. Do not include
+#'   any other files in the ZIP file.
+#' 
+#' 7. Submit the ZIP file to the assignment platform for evaluation.
+#' 
+#' ## Minimum Requirements to pass assignment
+#' 
+#' - **Headers**: The template seems a bit tame, so make sure to add some
+#'  headers to the page. Add headers using bootstrap classes, not simply adding
+#'  h1, h2, etc. tags.
+#' 
+#' - **Icons**: Add some icons to the page. You can use Bootstrap icons are
+#' activated in the template, chose the ones that best fit the theme of the
+#' course and add them to parts of the page which seems reasonable.
+#' 
+#' - **Cards**: The cards seems a bit... off? Make sure to add some proper card
+#' classes to the cards to make them look better.
+#' 
+#' - **Buttons**: The buttons are a bit boring. Add some classes to the buttons
+#' to make them look more interesting. Also, make sure you can actually identify
+#' them as actual buttons!
+#' 
+#' - **Accordion**: The accordion looks decent, but why are they not
+#' interactive? Make sure to add the correct classes to make sure only one
+#' accordion is open at a time when clicked.
+#' 
+#' - **List Group**: I am of the opinion that the review section would benefit
+#' from a list group. Add a list group to the review section, with proper
+#' implementation.
+#' 
+#' - **Modals**: In the end of the document you will find a section to get the
+#' latest updates. This sections looks AWFUL! Add a modal to the section to make
+#' it look better and add some interactivity. Please note that given that more
+#' divs will not be needed, work with what you have.
+#' 
+#' - **Offcanvas**: The button "Check my other courses" is meant to open an
+#' offcanvas. If you inspect the HTML you will see that the offcanvas is already
+#' there, but it is not working. You will find the offcanvas at the end of the
+#' document. Make sure to add the correct classes to the button to make it work,
+#' as well.
+#' 
+#' ## Grading Criteria
+#' 
+#' - 6 out of 8 requirements is expected to be met adaquately, in order to pass
+#'   the assignment.
+#' 
+#' - The assignment will be graded based on the completion of the minimum
+#'   requirements, and quality of implementation will dictate whether an certain
+#'   requirement is met.
+#' 
+#' - Comments will be provided upon completion of the assignment, detailing
+#'   which requirements were met and which were not.
+#' 
+#' ## Resources
+#' 
+#' - [bootstrap-5-tutorial
+#'   repository](https://github.com/clg-du/bootstrap-5-tutorial)
+#' 
+
